8c55814e3629c03daa702f444fb68c83295d35bb
Frank Mueller <frank.mueller@themue.dev>
2021-05-02 21:10:50 +0200

    Test of continuation of samples development

--- pkg/services/doc.go
+++ pkg/services/doc.go
@@ -1,6 +1,6 @@
 // The Samples Project
 //
-// Copyright 2020 Frank Mueller / Oldenburg / Germany / World
+// Copyright 2020-2021 Frank Mueller / Oldenburg / Germany / World
 //
 // All rights reserved. Use of this source code is governed
 // by the new BSD license.
--- pkg/services/services_test.go
+++ pkg/services/services_test.go
@@ -1,9 +1,10 @@
 // The Samples Project
 //
-// Copyright 2020 Frank Mueller / Oldenburg / Germany / World
+// Copyright 2020-2021 Frank Mueller / Oldenburg / Germany / World
 //
 // All rights reserved. Use of this source code is governed
 // by the new BSD license.
+
 package services_test
 
 import (
